01ea38f3609771d0c59d33e2e42f870960400811
laixiangran <laixiangran@163.com>
2017-04-15 23:57:08 +0800

    move files

--- example/app/app.module.ts
+++ example/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {EssenceNg2ViewerModule} from "../../../src";
+import {EssenceNg2ViewerModule} from "../../src/essence-ng2-viewer.module";
 
 @NgModule({
     imports: [
