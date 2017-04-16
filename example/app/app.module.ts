00a6c233fed5cfbb163c58f3ef6c15a0e702c018
laixiangran <laixiangran@163.com>
2017-04-16 13:02:01 +0800

    move files

--- example/app/app.module.ts
+++ example/app/app.module.ts
@@ -3,7 +3,6 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {EssenceNg2ViewerModule} from "../../src/essence-ng2-viewer.module";
-import { TableModule } from 'ng2-pack';
 
 @NgModule({
     imports: [
